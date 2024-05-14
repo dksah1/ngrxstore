9fa70c013da9d3a1ee03ab8d9cb7ef357078f02e
dksah1 <dkboss127126@gmail.com>
2024-05-15 00:09:52 +0545

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideState, provideStore } from '@ngrx/store';
+import { counterReducer } from './states/counter/counter.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+    provideStore(),
+    provideState({ name: 'çounter', reducer: counterReducer }),
+  ],
 };
